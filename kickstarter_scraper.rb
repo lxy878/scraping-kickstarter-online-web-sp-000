84da0c53c5526d782e83c289676745a31af7f76b
Xiaoyi Liu <lxy878@gmail.com>
2020-03-15 11:07:58 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,11 +6,12 @@ def create_project_hash
   # write your code here
   html = File.read("./fixtures/kickstarter.html")
   doc = Nokogiri::HTML(html)
-  kckstarter = doc.css("li.project.grid_4")
-
-  projects = {}
-  
-  hash
+  kickstarter = doc.css("li.project.grid_4")
+  # projects = {}
+  # kickstarter.each do |project|
+  #   project
+  # end
+  # projects
 end
 
 puts create_project_hash
