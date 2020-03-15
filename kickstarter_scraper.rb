c0c7a230e664db7db1b531a3d7660aa1daffb0ff
Xiaoyi Liu <lxy878@gmail.com>
2020-03-15 10:36:59 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,8 +5,10 @@ require 'pry'
 def create_project_hash
   # write your code here
   html = File.read("./fixtures/kickstarter.html")
-  hash = {}
+  doc = Nokogiri:HTML(html)
   
+  hash = {}
+
 
   hash
 end
