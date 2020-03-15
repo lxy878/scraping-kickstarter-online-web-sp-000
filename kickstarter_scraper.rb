e53120e7c2448de0870810928deae6f0a70717a0
Xiaoyi Liu <lxy878@gmail.com>
2020-03-15 11:15:47 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,7 +9,10 @@ def create_project_hash
   kickstarter = doc.css("li.project.grid_4").first
 
   # title = kickstarter.css("h2.bbcard_name strong a").text
-  
+  # :image_link => pry(main)> kickstarter.css("div.project-thumbnail a img").attribute("src").value
+  #   :description => "Description",
+  #   :location => "Location",
+  #   :percent_funded => "Percent Funded"
   binding.pry
   # projects = {}
   # kickstarter.each do |project|
