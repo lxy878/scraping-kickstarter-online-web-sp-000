6ccb72c40e6f307cf54e71cfeebe6b0f2acedb21
Xiaoyi Liu <lxy878@gmail.com>
2020-03-15 11:36:08 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,14 +6,8 @@ def create_project_hash
   # write your code here
   html = File.read("./fixtures/kickstarter.html")
   doc = Nokogiri::HTML(html)
-  kickstarter = doc.css("li.project.grid_4").first
-
-  # title = kickstarter.css("h2.bbcard_name strong a").text
-  # :image_link => kickstarter.css("div.project-thumbnail a img").attribute("src").value
-  # :description => kickstarter.css("p.bbcard_blurb").text.strip
-  # :location => kickstarter.css("ul.project-meta span.location-name").text
-  # :percent_funded => kickstarter.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
   projects = {}
+  kickstarter = doc.css("li.project.grid_4")
   kickstarter.each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
